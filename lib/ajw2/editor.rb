c6a32512a579fc42a16fbcec94a7ebeca8142ff5
dtan4 <dtanshi45@gmail.com>
2013-10-31 10:01:30 +0900

    Initial commit

---
+++ lib/ajw2/editor.rb
@@ -0,0 +1,7 @@
+require "ajw2/editor/version"
+
+module Ajw2
+  module Editor
+    # Your code goes here...
+  end
+end
---
+++ lib/ajw2/editor/version.rb
@@ -0,0 +1,5 @@
+module Ajw2
+  module Editor
+    VERSION = "0.0.1"
+  end
+end
